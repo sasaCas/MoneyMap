a5a2ae8e010959608e72e75e113845b3428d9bea
mac <x>
2018-04-17 21:28:15 +0200

    FUNCIONALIDAD adding IMPLEMENTADA. Damos funcionalidad a la vista codificando su lógica. Funciona pero aún no puedo grabar un registro y otro y otro. Para grabar, debo volver a la principal con el botón de back y luego volver a pulsar el botón de , + , y luego el de , AGREGAR. No puedo agregar y luego volver a escribir y agregar otra vez. Si lo intento no actualiza por un error , Unhandled rejection: ConstraintError: Key already exists in the object store. ,. . ATENTOS [(ngModel)]="model.title" . Si se hace tal cual dice el tutorial no funciona y arroja un fallo ya que no reconoce el objeto , model , recién declarado. Por eso lo inicializamos directamente en su declaración tal y como se ve en mi código aquí mostrado.

--- src/pages/adding/adding.ts
+++ src/pages/adding/adding.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+// Para dar funcionalidad a esta vista tenemos que hacer uso del
+// modelo. Para ello lo primero es importarlo. Nuestro modelo está
+// dentro del archivo database, NO confundir con TransactionPage
+import { Transaction } from '../../database';
+
+
 /**
  * Generated class for the AddingPage page.
  *
@@ -15,11 +21,29 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class AddingPage {
 
+  // Agregamos una propiedad que vamos a llamar , model ,.
+  // Me he visto obligado a inicializar la variable aquí mismo
+  // y no dentro del , ionViewDidLoad() , como se ve en el vídeo.
+  // Hay más gente igual y esto parece que resuelve el problema.
+  model : Transaction = new Transaction(null, "");
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad AddingPage');
+    // construímos el objeto recién declarado , model , y para
+    // eso voy a pegar aquí lo que nos pide el constructor:
+    // constructor(amount:number, title:string, lat?:number, lng?:number,
+    //             id?:number, imageUrl?:string)
+      // Según el vídeo aquí debería de construir el objeto model
+      // pero he tenido que nombrarlo fuera, justo en la clase
+      // porque aquí no se construía el objeto. Lo que voy a hacer es
+      // asignarle unos valores para probarlo.
+
+  }
+
+  save(){
+    this.model.save();
   }
 
 }
